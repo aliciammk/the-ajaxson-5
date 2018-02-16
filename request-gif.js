38762fe4a14cb889835e7de9fa0f248595ad0822
Alicia <aliciakramme@gmail.com>
2018-02-16 15:01:17 -0600

    implement core functionality

--- request-gif.js
+++ request-gif.js
@@ -19,17 +19,17 @@ function fetchAndDisplayGif(event) {
     event.preventDefault();
     
     // get the user's input text from the DOM
-    var searchQuery = ""; // TODO should be e.g. "dance"
+    var searchQuery = $("input[name='tag']").val(); // TODO should be e.g. "dance"
 
     // configure a few parameters to attach to our request
     var params = { 
         api_key: "dc6zaTOxFJmzC", 
-        tag : "" // TODO should be e.g. "jackson 5 dance"
+        tag : "Jackson 5 " + searchQuery // TODO should be e.g. "jackson 5 dance"
     };
     
     // make an ajax request for a random GIF
     $.ajax({
-        url: "", // TODO where should this request be sent?
+        url: "https://api.giphy.com/v1/gifs/random", // TODO where should this request be sent?
         data: params, // attach those extra parameters onto the request
         success: function(response) {
             // if the response comes back successfully, the code in here will execute.
@@ -40,7 +40,12 @@ function fetchAndDisplayGif(event) {
             
             // TODO
             // 1. set the source attribute of our image to the image_url of the GIF
+            var imgSrc = response.data.image_url;
+
             // 2. hide the feedback message and display the image
+            $("#gif").attr('src',imgSrc);
+            setGifLoadedStatus(true);
+            $("#feedback").text("");
         },
         error: function() {
             // if something went wrong, the code in here will execute instead of the success function
